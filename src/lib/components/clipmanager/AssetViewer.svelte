b2db11c81f14d8d5cc888581b58e7fcb041973e4
zonetecde <zonetecde@gmail.com>
2024-03-19 18:48:16 +0100

    + better audio and video asset viewer

--- src/lib/components/clipmanager/AssetViewer.svelte
+++ src/lib/components/clipmanager/AssetViewer.svelte
@@ -3,21 +3,34 @@
 	import { convertFileSrc } from '@tauri-apps/api/tauri';
 
 	export let asset: Asset;
+
+	let isHovered = false;
 </script>
 
 <div
-	class="flex items-center justify-between p-2 border-b-2 flex-col max-w-full border-4 border-[#423f3f] rounded-2xl py-4 bg-[#0f0e0e]"
+	class="group flex items-center justify-between p-2 border-b-2 flex-col max-w-full border-4 border-[#423f3f] rounded-2xl py-4 bg-[#0f0e0e]"
+	on:mouseenter={() => (isHovered = true)}
+	on:mouseleave={() => (isHovered = false)}
+	role="contentinfo"
 >
 	{#if asset.type === 'video'}
-		<video src={convertFileSrc(asset.filePath)} class="w-full object-cover" controls>
+		<video src={convertFileSrc(asset.filePath)} class="w-full object-cover" controls={isHovered}>
 			<track kind="captions" />
 		</video>
 	{:else if asset.type === 'image'}
 		<img src={convertFileSrc(asset.filePath)} class="w-full object-cover" alt={asset.filePath} />
 	{:else if asset.type === 'audio'}
-		<img src="/icons/audio.png" class="w-full h-28 object-cover" alt={asset.filePath} />
+		<img
+			src="/icons/audio.png"
+			class="w-full h-16 object-contain block group-hover:hidden"
+			alt={asset.filePath}
+		/>
 
-		<audio src={convertFileSrc(asset.filePath)} class="w-full object-cover" controls></audio>
+		<audio
+			src={convertFileSrc(asset.filePath)}
+			class="w-full h-16 object-cover hidden group-hover:block"
+			controls
+		></audio>
 	{:else}
 		<p class="text-white">Unsupported file type</p>
 	{/if}
