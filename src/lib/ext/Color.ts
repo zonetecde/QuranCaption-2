b4d59680e4416a2612d8ca9c1c5656fa4a017eea
zonetecde <zonetecde@gmail.com>
2024-05-01 12:51:54 +0200

    + context menu

---
+++ src/lib/ext/Color.ts
@@ -0,0 +1,19 @@
+export function generateBlueColor(uniqueId: string): string {
+	const hash = uniqueId.split('').reduce((acc, char) => char.charCodeAt(0) + ((acc << 5) - acc), 0);
+
+	const r = ((hash & 0x000000ff) % 40) + 83;
+	const g = (((hash & 0x0000ff00) >> 8) % 40) + 140;
+	const b = (((hash & 0x00ff0000) >> 16) % 40) + 150;
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function generatePinkColor(uniqueId: string): string {
+	const hash = uniqueId.split('').reduce((acc, char) => char.charCodeAt(0) + ((acc << 5) - acc), 0);
+
+	const r = ((hash & 0x000000ff) % 40) + 200;
+	const g = (((hash & 0x0000ff00) >> 8) % 40) + 83;
+	const b = (((hash & 0x00ff0000) >> 16) % 40) + 140;
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
