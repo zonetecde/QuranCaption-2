6a02802cca5dd7b001b9a3411937004852c26931
Rayane STASZEWSKI <zonetecde@gmail.com>
2025-05-04 10:13:29 +0200

    Updated software version to 4.2

--- src/lib/ext/GlobalVariables.ts
+++ src/lib/ext/GlobalVariables.ts
@@ -1,4 +1,4 @@
-export const SOFTWARE_VERSION = 'v4.1';
+export const SOFTWARE_VERSION = 'v4.2';
 export const GITHUB_API_URL =
 	'https://api.github.com/repos/zonetecde/QuranCaption-2/releases/latest';
 export const GITHUB_REPO_LINK = 'https://github.com/zonetecde/QuranCaption-2';
