c53e2930392f2a7217505f3111c61b9965697ac1
Rayane STASZEWSKI <zonetecde@gmail.com>
2025-05-23 15:22:30 +0200

    + fixed performance issue with long vid

--- src/lib/components/timeline/Timeline.svelte
+++ src/lib/components/timeline/Timeline.svelte
@@ -47,11 +47,9 @@
 		}
 	}
 
-	function moveCursorToMousePosition(e: any, i: number) {
-		let rect = e.currentTarget.getBoundingClientRect();
-		const positionClickedWithin = e.clientX - rect.left;
-		const totalSize = $zoom;
-		const toAdd = (positionClickedWithin / totalSize) * 1000;
+	function calculateCursorPositionFromMouse(mouseXrelative: number) {
+		const newCursorPosition = (mouseXrelative / $zoom) * 1000;
+
 		// Enlève temporairement l'effet de fade (car on ne veut pas que le fade soit visible lors du déplacement du curseur, juste
 		// lors de la lecture)
 		const temp = $currentProject.projectSettings.globalSubtitlesSettings.fadeDuration;
@@ -62,8 +60,8 @@
 			}, 0);
 		}
 
-		cursorPosition.set((e.clientX - rect.left) / $zoom + i * 1000 + toAdd);
-		setCurrentVideoTime.set($cursorPosition);
+		cursorPosition.set(newCursorPosition);
+		setCurrentVideoTime.set(newCursorPosition);
 
 		// Permet de forcer la mise à jour du clip en cours de lecture
 		setTimeout(() => {
@@ -84,55 +82,69 @@
 	on:wheel={handleMouseWheelWheeling}
 >
 	<div class={'h-full bg-[#1f1d1d] relative w-max ' + (useInPlayer ? 'content' : '')}>
-		<div class="pl-24 lg:pl-40 h-full flex w-full">
+		<div class="pl-24 lg:pl-40 h-full relative">
 			{#if !$bestPerformance}
-				{#each Array.from({ length: timeLineTotalDuration }, (_, i) => i) as i}
-					<div style="min-width: {$zoom}px; max-width: {$zoom}px;">
-						{#if $zoom >= 30 || ($zoom > 20 && i % 5 === 0 && $zoom < 30) || ($zoom > 10 && i % 3 === 0 && $zoom < 20) || ($zoom > 5 && i % 5 === 0 && $zoom < 10) || ($zoom > 3 && i % 10 === 0 && $zoom < 5) || ($zoom > 1.5 && i % 20 === 0 && $zoom < 3) || ($zoom > 1 && i % 50 === 0 && $zoom < 1.5)}
-							<p class="text-[0.6rem] opacity-30 -translate-x-1/2 w-fit">
-								{secondsToHHMMSS(i, true)[0]}
-							</p>
-						{/if}
-
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<div
-							class="w-full h-6 absolute top-0 z-10 select-none outline-none cursor-pointer"
-							tabindex="-1"
-							on:mousedown={(e) => moveCursorToMousePosition(e, i)}
-							on:mousemove={(e) => {
-								if (e.buttons !== 1) return;
-								moveCursorToMousePosition(e, i);
-							}}
-						></div>
+				<div class="relative" style="width: {timeLineTotalDuration * $zoom}px; height: 100%;">
+					<!-- Background Layer -->
+					<div
+						class="absolute top-0 left-0 h-full w-full"
+						style={`background: repeating-linear-gradient(
+									90deg,
+									#1a1e1d,
+									#1a1e1d ${$zoom}px,
+									#1d1d1d ${$zoom}px,
+									#1d1d1d ${$zoom * 2}px
+								);
+								z-index: 1;`}
+					></div>
+					<!-- Time Markers Layer -->
+					<div class="absolute top-0 left-0 h-6 w-full" style="z-index: 2;">
+						{#each Array.from({ length: timeLineTotalDuration }, (_, i) => i) as i}
+							{#if $zoom >= 30 || ($zoom > 20 && i % 5 === 0 && $zoom < 30) || ($zoom > 10 && i % 3 === 0 && $zoom < 20) || ($zoom > 5 && i % 5 === 0 && $zoom < 10) || ($zoom > 3 && i % 10 === 0 && $zoom < 5) || ($zoom > 1.5 && i % 20 === 0 && $zoom < 3) || ($zoom > 1 && i % 50 === 0 && $zoom < 1.5)}
+								<p
+									class="text-[0.6rem] opacity-30 absolute -translate-x-1/2 w-fit"
+									style="left: {i * $zoom + $zoom / 2}px; top: 0;"
+								>
+									{secondsToHHMMSS(i, true)[0]}
+								</p>
+							{/if}
+						{/each}
 					</div>
-				{/each}
-
-				<!-- lignes verticales -->
-				<div
-					class="h-full"
-					style={`width: ${timeLineTotalDuration * $zoom}px;
-							background: repeating-linear-gradient(
-								90deg,          
-								#1a1e1d,           
-								#1a1e1d ${$zoom}px,   
-								#1d1d1d ${$zoom}px,   
-								#1d1d1d ${$zoom * 2}px    
-							);`}
-				></div>
+					<!-- Clickable Interaction Layer -->
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<div
+						class="absolute top-0 left-0 w-full h-6 select-none outline-none cursor-pointer"
+						style="z-index: 10;"
+						tabindex="-1"
+						on:mousedown={(e) => {
+							let rect = e.currentTarget.getBoundingClientRect();
+							calculateCursorPositionFromMouse(e.clientX - rect.left);
+						}}
+						on:mousemove={(e) => {
+							if (e.buttons !== 1 || $isPreviewPlaying) return;
+							let rect = e.currentTarget.getBoundingClientRect();
+							calculateCursorPositionFromMouse(e.clientX - rect.left);
+						}}
+					></div>
+				</div>
 			{:else}
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<div
-					class="h-4 absolute top-0 z-10 select-none outline-none cursor-pointer"
-					tabindex="-1"
-					style="width: {timeLineTotalDuration * $zoom}px;"
-					on:mousedown={(e) => {
-						moveCursorToMousePosition(e, 0);
-					}}
-					on:mousemove={(e) => {
-						if (e.buttons !== 1 || $isPreviewPlaying) return;
-						moveCursorToMousePosition(e, 1);
-					}}
-				></div>
+				<div class="relative" style="width: {timeLineTotalDuration * $zoom}px; height: 100%;">
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<div
+						class="absolute top-0 left-0 h-4 w-full select-none outline-none cursor-pointer"
+						style="z-index: 10;"
+						tabindex="-1"
+						on:mousedown={(e) => {
+							let rect = e.currentTarget.getBoundingClientRect();
+							calculateCursorPositionFromMouse(e.clientX - rect.left);
+						}}
+						on:mousemove={(e) => {
+							if (e.buttons !== 1 || $isPreviewPlaying) return;
+							let rect = e.currentTarget.getBoundingClientRect();
+							calculateCursorPositionFromMouse(e.clientX - rect.left);
+						}}
+					></div>
+				</div>
 			{/if}
 			<!-- Cursor -->
 			<!-- The `- 1` in the calcul is because the cursor is 2px thick -->
@@ -146,7 +158,7 @@
 				<!-- Selection area between start and end time -->
 				{#if $startTime >= 0 && ($endTime || getVideoDurationInMs()) > $startTime}
 					<div
-						class="absolute top-5 left-24 lg:left-40 h-60 z-50"
+						class="absolute top-5 left-0 h-60 z-50"
 						style="
 						transform: translateX({($startTime / 1000) * $zoom}px);
 						width: {((($endTime || getVideoDurationInMs()) - $startTime) / 1000) * $zoom}px;
@@ -160,7 +172,7 @@
 				<!-- Start time marker -->
 				{#if $startTime > 0}
 					<div
-						class="absolute top-5 left-24 lg:left-40 h-60 z-50 flex items-center border-l-2 border-[#229753]"
+						class="absolute top-5 left-0 h-60 z-50 flex items-center border-l-2 border-[#229753]"
 						style="transform: translateX({($startTime / 1000) * $zoom}px);"
 					></div>
 				{/if}
@@ -168,7 +180,7 @@
 				<!-- End time marker -->
 				{#if ($endTime || getVideoDurationInMs()) > 0}
 					<div
-						class="absolute top-5 left-24 lg:left-40 h-60 z-50 flex items-center border-l-2 border-[#229753]"
+						class="absolute top-5 left-0 h-60 z-50 flex items-center border-l-2 border-[#229753]"
 						style="transform: translateX({(($endTime || getVideoDurationInMs()) / 1000) *
 							$zoom}px);"
 					></div>
