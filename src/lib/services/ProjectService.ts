6943c9686779432bfbb5e8f21e9756bf13e27675
Rayane STASZEWSKI <zonetecde@gmail.com>
2025-08-10 02:41:15 +0200

    Correction de tout les styles qui ne fonctionnaient pas bien

--- src/lib/services/ProjectService.ts
+++ src/lib/services/ProjectService.ts
@@ -83,7 +83,7 @@ export class ProjectService {
 
 		// Si le projet ne contient pas de styles vidéo, on initialise avec un style par défaut
 		// || true
-		if (Object.keys(project.content.videoStyle.styles).length === 0 || true) {
+		if (Object.keys(project.content.videoStyle.styles).length === 0) {
 			// Si les styles ne sont pas définis, on initialise avec un style par défaut
 			project.content.videoStyle = await VideoStyle.setDefaultStylesToDefaultOne();
 		}
